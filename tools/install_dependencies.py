3f8dac10eaa787c397694d94fd8f6bd26a479174
lachlangrose <lachlan.grose@monash.edu>
2018-05-31 10:22:10 +1000

    Qgis3 installers

--- tools/install_dependencies.py
+++ tools/install_dependencies.py
@@ -52,15 +52,15 @@ class Installer():
             os.chdir('windows_installers')
             pip_install('mplstereonet')
             if struct.calcsize("P")*8 == 64:
-                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/Cython-0.27.2-cp27-cp27m-win_amd64.whl','Cython-0.27.2-cp27-cp27m-win_amd64.whl')
-                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/scikit_image-0.13.1-cp27-cp27m-win_amd64.whl','scikit_image-0.13.1-cp27-cp27m-win_amd64.whl')
-                pip_install('Cython-0.27.2-cp27-cp27m-win_amd64.whl')
-                pip_install('scikit_image-0.13.1-cp27-cp27m-win_amd64.whl')
+                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/Cython‑0.28.3‑cp36‑cp36m‑win_amd64.whl','Cython‑0.28.3‑cp36‑cp36m‑win_amd64.whl')
+                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/scikit_image‑0.14.0‑cp36‑cp36m‑win_amd64.whl','scikit_image‑0.14.0‑cp36‑cp36m‑win_amd64.whl')
+                pip_install('Cython‑0.28.3‑cp36‑cp36m‑win_amd64.whl')
+                pip_install('scikit_image‑0.14.0‑cp36‑cp36m‑win_amd64.whl')
             if struct.calcsize("P")*8==32:
-                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/Cython-0.27.2-cp27-cp27m-win32.whl','Cython-0.27.2-cp27-cp27m-win32.whl')
-                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/scikit_image-0.13.1-cp27-cp27m-win32.whl','scikit_image-0.13.1-cp27-cp27m-win32.whl')
-                pip_install('Cython-0.27.2-cp27-cp27m-win_amd64.whl')
-                pip_install('scikit_image-0.13.1-cp27-cp27m-win_amd64.whl')
+                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/Cython‑0.28.3‑cp36‑cp36m‑win32.whl','Cython‑0.28.3‑cp36‑cp36m‑win32.whl')
+                urllib.urlretrieve('https://github.com/lachlangrose/GeoTrace/raw/downloads/windows_installers/wheels/scikit_image‑0.14.0‑cp36‑cp36m‑win32.whl','scikit_image‑0.14.0‑cp36‑cp36m‑win32.whl')
+                pip_install('Cython‑0.28.3‑cp36‑cp36m‑win32.whl')
+                pip_install('scikit_image‑0.14.0‑cp36‑cp36m‑win32.whl')
             home_folder = os.path.expanduser("~")
             user_site_packages_folder = "{}\AppData\\Roaming\\Python\\Python27\\site-packages".format(home_folder)
             if user_site_packages_folder not in sys.path:
